7a6faaafacbe3b73bd2302db981f762c18524b8b
xugongkai-mac <xugongkai@163.com>
2024-04-27 20:10:55 +0800

    增加监控

--- src/main/java/com/xugongkai/GithubActionResearchApplication.java
+++ src/main/java/com/xugongkai/GithubActionResearchApplication.java
@@ -1,8 +1,12 @@
 package com.xugongkai;
 
+import cn.hutool.core.util.RuntimeUtil;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +31,9 @@ public class GithubActionResearchApplication {
         return "Hello, I am " + compilationMXBeanName + ", variables foo.bar=" + foobar +", have a good time!";
     }
 
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> meterRegistryMeterRegistryCustomizer(@Value("${spring.application.name:unknown}") String applicationName) {
+        int pid = RuntimeUtil.getPid();
+        return registry -> registry.config().commonTags("application", applicationName + "@" + pid);
+    }
 }
